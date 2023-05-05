1733ac5563063e9175755caaa45600c4135b9b1e
maxsavinmax <savin.maksim.savin@gmail.com>
2023-05-05 18:59:44 +0300

    feat: add fixed header

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function onScroll() {
+    const header = document.querySelector(".header");
+    const headerLogo = document.querySelector(".header__logo-img");
+
+    if (window.scrollY > 140) {
+        header.classList.add("header--scrolled");
+        headerLogo.classList.add("header__logo-img--scrolled");
+    } else {
+        header.classList.remove("header--scrolled");
+        headerLogo.classList.remove("header__logo-img--scrolled");
+    }
+}
+
+function addScrollListener() {
+    window.addEventListener("scroll", onScroll);
+}
+
+function removeScrollListener() {
+    window.removeEventListener("scroll", onScroll);
+}
+
+addScrollListener();
